522f26ecf1e5c9930677b85d45d232630afbc858
nurakbar-web <70342420+nurakbar-web@users.noreply.github.com>
2020-12-21 11:39:04 +0300

    Add files via upload

---
+++ дз Java 2 lesson 3/src/com/company/LimitException.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.security.PublicKey;
+
+public class LimitException extends Exception{
+    private double remainingAmount;
+    public LimitException(String message,double remainingAmount){
+        super(message);
+        this.remainingAmount=remainingAmount;
+    }
+
+    public double getRemainingAmount() {
+        return remainingAmount;
+    }
+
+}
